7debb10170e29dcadb87b0e415281d071b550bfb
LSanthosh01 <lsanthoshkumar01@gmail.com>
2024-03-09 23:55:45 +0530

    commit

--- PlacementManagement/src/main/java/com/tnsif/placement/repository/CertificateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.placement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.placement.entity.Certificate;
-
-public interface CertificateRepository extends JpaRepository<Certificate, Long> {
-
-}
